5eb9e029aede293ad0959391a5944f822452cc58
LuyenXuanTung <xuantung2113@gmail.com>
2024-10-30 01:25:06 +0700

    nha khoa project

---
+++ assets/vendor/toastify/custom.js
@@ -0,0 +1,12 @@
+
+Toastify({
+  text: "Welcome Xuân",
+  duration: 3000,
+  close: true,
+  avatar: 'assets/images/TNT Studio (962).jpg',
+  className: "bg-black",
+  offset: {
+    x: 9,
+    y: 65,
+  },
+}).showToast();
